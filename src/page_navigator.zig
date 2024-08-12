e69b51a3d0d6039f8aa39689c780733afef9d02c
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-08-12 01:02:40 +0100

    added 'embed_pictures' compile flag thanks to Allah

--- src/page_navigator.zig
+++ src/page_navigator.zig
@@ -2,6 +2,7 @@
 // la ilaha illa Allah Mohammed Rassoul Allah
 
 const std = @import("std");
+const compile_config = @import("compile_config");
 
 const sf = struct {
     const sfml = @import("sfml");
@@ -21,26 +22,35 @@ pub var bookmarks: [10]usize = [1]usize{0} ** 10;
 pub const surah_start_pages_list: [114]usize = [_]usize{ 1, 2, 50, 77, 106, 128, 151, 177, 187, 208, 221, 235, 249, 255, 262, 267, 282, 293, 305, 312, 322, 332, 342, 350, 359, 367, 377, 385, 396, 404, 411, 415, 418, 428, 434, 440, 446, 453, 458, 467, 477, 483, 489, 496, 499, 502, 507, 511, 515, 518, 520, 523, 526, 528, 531, 534, 537, 542, 545, 549, 551, 553, 554, 556, 558, 560, 562, 564, 566, 568, 570, 572, 574, 575, 577, 578, 580, 582, 583, 585, 586, 587, 587, 589, 590, 591, 591, 592, 593, 594, 595, 595, 596, 596, 597, 597, 598, 598, 599, 599, 600, 600, 601, 601, 601, 602, 602, 602, 603, 603, 603, 604, 604, 604 };
 pub const hizb_start_pages_list: [60]usize = [60]usize{ 1, 11, 22, 32, 43, 51, 62, 72, 82, 92, 102, 111, 121, 132, 142, 151, 162, 173, 182, 192, 201, 212, 222, 231, 242, 252, 262, 272, 282, 292, 302, 312, 322, 332, 342, 352, 362, 371, 382, 392, 402, 413, 422, 431, 442, 451, 462, 472, 482, 491, 502, 513, 522, 531, 542, 553, 562, 572, 582, 591 };
 
-fn embed_quran_pictures() [NUMBER_OF_PAGES][]const u8 {
-    var quran_pictures: [NUMBER_OF_PAGES][]const u8 = undefined;
+fn embed_quran_pictures() ?[NUMBER_OF_PAGES][]const u8 {
+    if (compile_config.embed_pictures) {
+        var quran_pictures: [NUMBER_OF_PAGES][]const u8 = undefined;
 
-    var i: usize = 0;
-    while (i < NUMBER_OF_PAGES) : (i += 1) {
-        var file_name_buffer: [64]u8 = undefined;
-        const file_name_slice = std.fmt.bufPrint(&file_name_buffer, "res/{d}-scaled.jpg", .{i + 1}) catch |e| {
-            // std.log.err("alhamdo li Allah error while writing to 'file_name_buffer': {any}\n", .{e});
-            @compileLog("alhamdo li Allah error ", e, " while writing to 'file_name_buffer' ", i);
-            @compileError("error while writing file name to buffer\n");
-        };
+        var i: usize = 0;
+        while (i < NUMBER_OF_PAGES) : (i += 1) {
+            var file_name_buffer: [64]u8 = undefined;
+            const file_name_slice = std.fmt.bufPrint(&file_name_buffer, "res/{d}-scaled.jpg", .{i + 1}) catch |e| {
+                // std.log.err("alhamdo li Allah error while writing to 'file_name_buffer': {any}\n", .{e});
+                @compileLog("alhamdo li Allah error ", e, " while writing to 'file_name_buffer' ", i);
+                @compileError("error while writing file name to buffer\n");
+            };
 
-        const file = @embedFile(file_name_slice);
-        quran_pictures[i] = file;
-    }
+            const file = @embedFile(file_name_slice);
+            quran_pictures[i] = file;
+        }
 
-    return quran_pictures;
+        return quran_pictures;
+    } else return null;
 }
 
-const quran_pictures_arr = embed_quran_pictures();
+const quran_pictures_arr = if (compile_config.embed_pictures) embed_quran_pictures().? else undefined;
+// list of possible image lcations
+// "res/{d}-scaled.jpg",
+// "src/res/{d}-scaled.jpg",
+// "/usr/share/quran-warsh/{d}-scaled.jpg",
+// "res/{d}.jpg",
+// "src/res/{d}.jpg",
+// "/usr/share/quran-warsh/{d}.jpg",
 
 /// sets the page diplayed starting from 1
 pub fn goToPage(sprite: *sf.Sprite, target_page: usize) void {
@@ -49,7 +59,21 @@ pub fn goToPage(sprite: *sf.Sprite, target_page: usize) void {
     if (sprite.getTexture()) |texture| {
         sf.c.sfTexture_destroy(@constCast(texture._const_ptr));
     }
-    sprite.setTexture(sf.Texture.createFromMemory(quran_pictures_arr[target_page - 1], .{ .top = 0, .left = 0, .width = 0, .height = 0 }) catch unreachable);
+
+    if (compile_config.embed_pictures) {
+        sprite.setTexture(sf.Texture.createFromMemory(quran_pictures_arr[target_page - 1], .{ .top = 0, .left = 0, .width = 0, .height = 0 }) catch unreachable);
+    } else {
+        var file_name_buffer: [64:0]u8 = [1:0]u8{0} ** 64;
+        const file_name_slice = std.fmt.bufPrintZ(&file_name_buffer, "src/res/{d}-scaled.jpg", .{target_page}) catch |e| {
+            std.log.err("alhamdo li Allah error while writing to 'file_name_buffer': {any} target_page {d}\n", .{ e, target_page });
+            return;
+        };
+
+        sprite.setTexture(sf.Texture.createFromFile(file_name_slice) catch |e| {
+            std.log.err("alhamdo li Allah error while callings 'sf.Texture.createFromFile()' err: '{any}' \n", .{e});
+            return;
+        });
+    }
 
     // sprite.setTextureRect(sf.IntRect.init(393, 170, 1360, 2184));
     // sprite.setTextureRect(sf.IntRect.init(196, 85, 680, 1542));
